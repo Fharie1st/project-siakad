25af1123ded3293819241972cb7f476ef05e281a
Fahriel Fauzi <fahrielfauzi24@gmail.com>
2026-07-05 16:45:47 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,26 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Paginator::useTailwind();
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
